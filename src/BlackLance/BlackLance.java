a95c6fa1ae6b6cf01a73a078d72edb9400cade41
Redmancometh <Brendanstcurry@gmail.com>
2014-08-10 16:58:48 -0500

    Fixed formatting changes, de-implemented maven

--- src/BlackLance/BlackLance.java
+++ src/BlackLance/BlackLance.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
 
+import Alchemy.HerbloreListeners;
 import Listeners.*;
 
 import org.bukkit.Bukkit;
@@ -35,7 +36,7 @@ public class BlackLance extends JavaPlugin
     static Connection c;
     public void onEnable()
     {
-	Bukkit.broadcastMessage("RELOADING TEST FOUR");
+	Bukkit.broadcastMessage("RELOADING TEST FIVE");
 	pl=this;
 	MySQL=new MySQL((Plugin) this, "localhost", "3306", "RPG", "root", "enter11284");
 	MySQL.openConnection();
@@ -56,11 +57,13 @@ public class BlackLance extends JavaPlugin
 	pm.registerEvents(new NPCListeners(this), this);
 	pm.registerEvents(new SmeltingListeners(), this);
 	pm.registerEvents(new SmithingListeners(), this);
+	pm.registerEvents(new HerbloreListeners(), this);
 	pm.registerEvents(new BlockListeners(this), this);
 	pm.registerEvents(new MenuListeners(this), this);
 	pm.registerEvents(new ItemListeners(this), this);
 	pm.registerEvents(new ConsumableListener(this), this);
 	pm.registerEvents(new TradeListener(), this);
+	pm.registerEvents(new Alchemy.MenuListeners(), this);
 	getCommand("horse").setExecutor(new CommandParser());
 	getCommand("setup").setExecutor(new CommandParser());
 	getCommand("blevel").setExecutor(new CommandParser());
